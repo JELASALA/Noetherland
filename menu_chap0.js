9ead7b5f6d4406c13a563e8cbe8ebb411a87009c
John LaSala <134564994+JELASALA@users.noreply.github.com>
2023-07-28 11:25:13 -0400

    Update menu_chap0.js

--- menu_chap0.js
+++ menu_chap0.js
@@ -4,18 +4,18 @@ var chap0  = new Array();  // This is for the Chap1 Dropdown Menu lists.
 
 // ==================== These HTML lines will be written ==================== //
 chap0[0]   = '<div id="chap0">';
-chap0[1]   = '<button class="dropdown-btn",style="text-align: center;">HOME<i class="fa fa-caret-down"></i></button>';
+chap0[1]   = '<button class="dropdown-btn",style="text-align: center">HOME<i class="fa fa-caret-down"></i></button>';
 chap0[2]   = '<div class="dropdown-container">';
-chap0[3]   = '<a href="#"><pre style="font-family: Helvetica; font-size: 14px;margin: 0px; padding: 0px;">0.1 Intro A B C D E F G H</pre></a>';
-chap0[4]   = '<a href="#"><pre style="font-family: Helvetica; font-size: 14px;margin: 0px; padding: 0px;">0.2 Teaching Philosophies</pre></a>';
-chap0[5]   = '<button class="dropdown-btn"><pre style="font-family: Helvetica; font-size: 14px;margin: 0px; padding: 0px;">0.3 Math You Need<i class="fa fa-caret-down"></i></pre></button>';
+chap0[3]   = '<a href="#">Introduction</a>';
+chap0[4]   = '<a href="#">Teaching Philosophies</a>';
+chap0[5]   = '<button class="dropdown-btn">Math You Need<i class="fa fa-caret-down"></i></button>';
 chap0[6]   = '<div class="dropdown-container">';
 chap0[7]   = '<a href="#">0.3.1 Calculus of Variations</a>';
 chap0[8]   = '<a href="#">0.3.2 Ordinary and Partial Differetial Equations</a>';
 chap0[9]   = '<a href="#">0.3.3 Method of Characteristics</a>';
 chap0[10]   = '</div>';
-chap0[11]   = '<a href="#"><pre style="font-family: Helvetica; font-size: 14px;margin: 0px; padding: 0px;">0.4 How to Study this e-torial</pre></a>';
-chap0[12]   = '<a href="#"><pre style="font-family: Helvetica; font-size: 14px;margin: 0px; padding: 0px;">0.5 Sources and References</pre></a></a>';
+chap0[11]   = '<a href="#">How to Study this E-tutorial</a>';
+chap0[12]   = '<a href="#">Sources and References</a></a>';
 chap0[13]   = '</div>';
 chap0[14]   = '</div><!-- Close CHAP0 -->';
 
