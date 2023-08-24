f5965c3644e5d4ed590356f62851dbad87bc1dca
John LaSala <134564994+JELASALA@users.noreply.github.com>
2023-08-24 19:41:51 -0400

    Update menu_chap8.js

--- menu_chap8.js
+++ menu_chap8.js
@@ -3,14 +3,14 @@
 var chap8  = new Array();  // This is for the Chap8 Dropdown Menu lists.
 
 // ==================== These HTML lines will be written ==================== //
-chap8[0]   = '<div id="chap6">';
-chap8[1]   = '<button class="dropdown-btn">6 Noethers Theorem <i class="fa fa-caret-down"></i></button>';
+chap8[0]   = '<div id="chap8">';
+chap8[1]   = '<button class="dropdown-btn">8 Quantum Field Theory <i class="fa fa-caret-down"></i></button>';
 chap8[2]   = '<div class="dropdown-container">';
 chap8[3]   = '<a href="#">8.1</a>';
 chap8[4]   = '<a href="#">8.2</a>';
 chap8[5]   = '<a href="#">8.3</a>';
 chap8[6]   = '</div>';
-chap8[7]   = '</div><!-- Close chap6 -->';
+chap8[7]   = '</div><!-- Close chap8 -->';
 
 function show(i)
  {
